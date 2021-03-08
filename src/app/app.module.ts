f637c2b39986039990a4685373d4cb2dd9c7678e
k11mounir <khadija11mounir@gmail.com>
2021-03-08 18:26:49 +0100

    saving side-bar and header

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,12 +3,12 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { DashboardComponent } from './layouts/dashboard/dashboard.component';
-import { WidgetComponent } from './layouts/widget/widget.component';
+import { DashboardModule } from './layouts/dashboard/dashboard.module';
 import { FooterComponent } from './shared/components/footer/footer.component';
 import { HeaderComponent } from './shared/components/header/header.component';
 import { SideBarComponent } from './shared/components/side-bar/side-bar.component';
 
+<<<<<<< HEAD
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { MenubarModule } from 'primeng/menubar';
 import { InputTextModule } from 'primeng/inputtext';
@@ -17,11 +17,12 @@ import { PanelMenuModule } from 'primeng/panelmenu';
 import { AvatarModule } from "primeng/avatar";
 import { BadgeModule } from "primeng/badge";
 
+=======
+import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
+>>>>>>> 9b5d2b9cfb9864f5998ae363ca8996c06759cc9f
 @NgModule({
   declarations: [
     AppComponent,
-    DashboardComponent,
-    WidgetComponent,
     FooterComponent,
     HeaderComponent,
     SideBarComponent
@@ -29,6 +30,7 @@ import { BadgeModule } from "primeng/badge";
   imports: [
     BrowserModule,
     AppRoutingModule,
+<<<<<<< HEAD
     BrowserAnimationsModule,
     MenubarModule,
     InputTextModule,
@@ -36,6 +38,10 @@ import { BadgeModule } from "primeng/badge";
     PanelMenuModule,
     AvatarModule,
     BadgeModule,
+=======
+    DashboardModule,
+    BrowserAnimationsModule
+>>>>>>> 9b5d2b9cfb9864f5998ae363ca8996c06759cc9f
   ],
   providers: [],
   bootstrap: [AppComponent]
