3e5f0a0ccc0b4047eb9f548a52e1c801e261b527
Benchari khaoula <46573696+khaoula1305@users.noreply.github.com>
2021-03-08 18:47:50 +0100

    sideBar and header (#2)
     adding side-bar and header

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,13 +3,19 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { DashboardModule } from './layouts/default/dashboard.module';
+import { DashboardModule } from './layouts/dashboard/dashboard.module';
 import { FooterComponent } from './shared/components/footer/footer.component';
 import { HeaderComponent } from './shared/components/header/header.component';
 import { SideBarComponent } from './shared/components/side-bar/side-bar.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { MenubarModule } from 'primeng/menubar';
+import { InputTextModule } from 'primeng/inputtext';
+import { ButtonModule } from 'primeng/button';
+import { PanelMenuModule } from 'primeng/panelmenu';
+import { AvatarModule } from "primeng/avatar";
+import { BadgeModule } from "primeng/badge";
 
 import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
-
 @NgModule({
   declarations: [
     AppComponent,
@@ -20,6 +26,13 @@ import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
   imports: [
     BrowserModule,
     AppRoutingModule,
+    BrowserAnimationsModule,
+    MenubarModule,
+    InputTextModule,
+    ButtonModule,
+    PanelMenuModule,
+    AvatarModule,
+    BadgeModule,
     DashboardModule,
     BrowserAnimationsModule
   ],
--- src/app/shared/components/header/header.component.ts
+++ src/app/shared/components/header/header.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import {MenubarModule} from 'primeng/menubar';
+import {MenuItem} from 'primeng/api';
 
 @Component({
   selector: 'app-header',
@@ -7,9 +9,34 @@ import { Component, OnInit } from '@angular/core';
 })
 export class HeaderComponent implements OnInit {
 
+  items: MenuItem[];
+
   constructor() { }
 
+
+
   ngOnInit(): void {
+    this.items = [
+      {
+          label:'User Name',
+          items:[
+              {
+                  label:'New',
+                  icon:'pi pi-fw pi-user-plus',
+
+              },
+              {
+                  label:'Delete',
+                  icon:'pi pi-fw pi-user-minus',
+
+              },
+              {
+                  label:'Search',
+                  icon:'pi pi-fw pi-users'
+              }
+          ]
+      }
+  ];
   }
 
 }
