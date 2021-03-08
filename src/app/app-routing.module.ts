b22a9b99244e06ffb5c43bbb3626ffc6e53265e9
Benchari khaoula <46573696+khaoula1305@users.noreply.github.com>
2021-03-08 18:31:17 +0100

    Dashboard and widgetPanel (#1)
    project structure and dependencies(primeNg, gridster, primIcon)
     configure dashboard and widgetPanel

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,12 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { AppComponent } from './app.component';
+import { DefaultComponent } from './layouts/default/default.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: '', component: DefaultComponent},
+  {path: 'widgetDetails', component: AppComponent}
+ ];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,24 +3,25 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { DashboardComponent } from './layouts/dashboard/dashboard.component';
-import { WidgetComponent } from './layouts/widget/widget.component';
+import { DashboardModule } from './layouts/default/dashboard.module';
 import { FooterComponent } from './shared/components/footer/footer.component';
 import { HeaderComponent } from './shared/components/header/header.component';
 import { SideBarComponent } from './shared/components/side-bar/side-bar.component';
 
+import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
+
 @NgModule({
   declarations: [
     AppComponent,
-    DashboardComponent,
-    WidgetComponent,
     FooterComponent,
     HeaderComponent,
     SideBarComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    DashboardModule,
+    BrowserAnimationsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
