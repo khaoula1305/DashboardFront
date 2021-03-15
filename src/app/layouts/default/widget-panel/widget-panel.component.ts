cc5a1a69cd32ba04591a66c31b03d1df89c012db
k11mounir <khadija11mounir@gmail.com>
2021-03-15 17:42:11 +0100

    add item fixed

--- src/app/layouts/default/widget-panel/widget-panel.component.ts
+++ src/app/layouts/default/widget-panel/widget-panel.component.ts
@@ -1,7 +1,9 @@
 import { Component, EventEmitter, Input, OnInit, Output } from '@angular/core';
+import { DashboardWidget } from 'src/app/models/dashboard-widget';
 import { Widget } from 'src/app/models/widget.model';
 import { DashboardWidgetService } from 'src/app/services/dashboard-widget.service';
 import { WidgetsService } from 'src/app/services/widgets.service';
+import { DashboardComponent } from '../dashboard/dashboard.component';
 
 @Component({
   selector: 'app-widget-panel',
@@ -42,7 +44,31 @@ export class WidgetPanelComponent implements OnInit {
 
     // permet d'ajouter un widget de widget-panel dans le dashboard
   addItem(widget : Widget){
-    //this.widgetDashboardService.addWidget({cols: 2, rows: 2, y: 0, x: 0, resizeEnabled:true, dragEnabled:true, type: widget.type});
+    debugger
+    let dashboardWidget:DashboardWidget=new DashboardWidget();
+    console.log(widget);
+    console.log(widget.title);
+
+   // dashboardWidget.id=5;
+
+    dashboardWidget.title=widget.title;
+    dashboardWidget.description=widget.description;
+    dashboardWidget.widget=widget;
+    dashboardWidget.x=1;
+    dashboardWidget.y=1;
+    dashboardWidget.cols=1;
+    dashboardWidget.rows=1;
+    dashboardWidget.dataSource=null;
+    dashboardWidget.dashboard=null;
+
+    console.log(dashboardWidget);
+    this.dashboardWidgetService.addDashboardWidget(dashboardWidget);
+    //this.dashboardWidgetService.getAllDashboardWidget();
+    this.dashboardWidgetService.getAllDashboardWidget().subscribe(
+      (data)=>{
+        console.log("lst modifs" + data);
+      }
+    )
   }
   // permet d'ajouter un nouveau widget generique
   addWidget() {
