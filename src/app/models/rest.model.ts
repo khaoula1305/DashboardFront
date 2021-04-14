a89de6faf63946b600b7ead4cf3fe411b2a43cbc
Khaoula BENCHARI <K. BENCHARI@involys.com>
2021-04-14 15:12:50 +0000

    Rest Token

--- src/app/models/rest.model.ts
+++ src/app/models/rest.model.ts
@@ -3,4 +3,7 @@ import { DataSource } from "./data-source.model";
 export class Rest extends DataSource{
     type:string;
     url: string;
+    login: string;
+    password: string;
+    token: string;
 }
